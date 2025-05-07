fb534016f350b87408143d93328cbd747b586449
Tralinge <46578807+Tralinge@users.noreply.github.com>
2025-05-07 15:49:21 +0800

    Update Characters.js

--- characters/Characters.js
+++ characters/Characters.js
@@ -1,6 +1,6 @@
-window.getCharacters = function () { return {
+window.getCharacters = function () { return [
     { name: "Mulan", avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Mulan.jpeg", blurb: "Woman who disguised herself as a man to fight in her father's place.", category: "Historical" },
     { name: "Wukong", avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Wukong.jpeg", blurb: "Woman who disguised herself as a man to fight in her father's place.", category: "Historical" },
     { name: "Nezha", avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Nezha.jpeg", blurb: "Woman who disguised herself as a man to fight in her father's place.", category: "Historical" },
-};
+];
 };

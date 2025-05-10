0afa9d30b61867fb021a0c86365067ba99c8361f
Tralinge <46578807+Tralinge@users.noreply.github.com>
2025-05-10 11:00:07 +0800

    Update Characters.js

--- characters/Characters.js
+++ characters/Characters.js
@@ -1,7 +1,7 @@
-window.getCharacters = function () { 
+window.getCharacters = function () {
     return {
         "Mulan": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Mulan.jpeg", blurb: "Folk heroine who disguised herself as a man to fight in her father's place against nomadic hordes", category: "Historical" },
         "Wukong": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Wukong.jpeg", blurb: "Also known as the Monkey King from Journey to the West; he has several supernatural abilities and is known as a bit of a trickster  ", category: "Historical" },
-        "Nezha": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Nezha.jpeg", blurb: "A taoist protection deity often show flying in the sky on wind fire wheels", category: "Historical" },
+        "Nezha": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Nezha.jpeg", blurb: "A taoist protection deity often show flying in the sky on wind fire wheels", category: "Historical" }
     };
 };

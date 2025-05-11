be1cdcd22cdac3212963dcdb7f331e533a719e93
Tralinge <46578807+Tralinge@users.noreply.github.com>
2025-05-11 18:35:07 +0800

    Update Characters.js

--- characters/Characters.js
+++ characters/Characters.js
@@ -3,5 +3,8 @@ window.getCharacters = function () {
         "Mulan": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Mulan.jpeg", blurb: "Folk heroine who disguised herself as a man to fight in her father's place against nomadic hordes", category: "Historical" },
         "Wukong": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Wukong.jpeg", blurb: "Also known as the Monkey King from Journey to the West; he has several supernatural abilities and is known as a bit of a trickster  ", category: "Historical" },
         "Nezha": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Nezha.jpeg", blurb: "A Taoist protection deity often show flying in the sky on wind fire wheels", category: "Historical" }
+        "Chang'e": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Chang'e.jpeg", blurb: "The moon goddess, known for her ethereal beauty and the white rabbit companion who joined her on her celestial journey after consuming the elixir of immortality.", category: "Creation Myth" },
+        "Houyi": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Houyi.jpeg", blurb: "A legendary archer of divine skill, famed for shooting down nine of the ten suns that scorched the earth and for his love story with Chang'e", category: "Creation Myth" },
+        "Cangjie": { avatarURL: "https://tralinge.github.io/ChineseDictionary.github.io/characters/Cangjie.jpeg", blurb: "A legendary figure credited with the invention of Chinese characters, often depicted with four eyes, symbolizing his extraordinary vision and ability to observe the patterns of nature that inspired writing.", category: "Creation Myth" }
     };
 };
